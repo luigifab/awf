0b9f02e6523a235226698b70f7916f3920436e32
Fabrice Creuzot <code@luigifab.fr>
2020-04-16 19:20:19 +0200

    copy arguments

--- awf-gtk3.sh
+++ awf-gtk3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-if ! ./awf-gtk3
+if ! ./awf-gtk3 "$@"
 then
     zenity --error --icon-name=awf --window-icon=awf --text="GTK 3 is not available."
 fi
--- awf-gtk4.sh
+++ awf-gtk4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-if ! ./awf-gtk4
+if ! ./awf-gtk4 "$@"
 then
     zenity --error --icon-name=awf --window-icon=awf --text="GTK 4 is not available."
 fi
