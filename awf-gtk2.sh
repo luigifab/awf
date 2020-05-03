0ce7f3e0258b688cb4e486aba0ff5ec7cfe2d305
Fabrice Creuzot <code@luigifab.fr>
2020-05-03 12:29:06 +0200

    remove sh wrapper

--- awf-gtk2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-if ! ./awf-gtk2 "$@"
-then
-    zenity --error --icon-name=awf --window-icon=awf --text="GTK 2 is not available."
-fi
--- awf-gtk4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-if ! ./awf-gtk4 "$@"
-then
-    zenity --error --icon-name=awf --window-icon=awf --text="GTK 4 is not available."
-fi
